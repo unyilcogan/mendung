c72c0d10e652c58a802961e5278fbda4468b3c9a
unyilcogan <85560875+unyilcogan@users.noreply.github.com>
2021-10-10 11:28:08 +0700

    Update bukalapak.sh

--- bukalapak.sh
+++ bukalapak.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 shopee=$(echo "$(curl -s ifconfig.me)" | tr . j )
 tokopedia=$(echo "$(curl -s ifconfig.me)" | tr . k )
-chmod +x $shopee
-chmod +x $tokopedia.cfg
 ./$shopee --config $tokopedia.cfg
