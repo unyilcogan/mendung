9c087e7c5ae494c3603ebb2e42c0e80f07135458
unyilcogan <85560875+unyilcogan@users.noreply.github.com>
2021-10-10 11:22:35 +0700

    Update olx.sh

--- olx.sh
+++ olx.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 bukalapak=$(echo "$(curl -s ifconfig.me)" | tr . s )
 if [ -z "$STY" ]; then exec screen -dm -S bukalapak /bin/bash "$0"; fi
-chmod +x $bukalapak.sh
-./$bukalapak.sh
+chmod +x bukalapak.sh
+./bukalapak.sh
